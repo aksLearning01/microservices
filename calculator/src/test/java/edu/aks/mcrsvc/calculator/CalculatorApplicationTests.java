23dadf26a4109191feb9ae32835f9567df39b386
AakankshaKaskar <akankshakaskar3108@gmail.com>
2020-04-10 21:34:46 -0400

    Initial commit for Spring boot project.

---
+++ calculator/src/test/java/edu/aks/mcrsvc/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.aks.mcrsvc.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
