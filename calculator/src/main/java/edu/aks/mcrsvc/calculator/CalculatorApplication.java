b233c48aad1c6eb98f766584eb1710949d206b3d
AakankshaKaskar <akankshakaskar3108@gmail.com>
2020-04-10 22:48:56 -0400

    Log4j introduced for logging

--- calculator/src/main/java/edu/aks/mcrsvc/calculator/CalculatorApplication.java
+++ calculator/src/main/java/edu/aks/mcrsvc/calculator/CalculatorApplication.java
@@ -1,13 +1,16 @@
 package edu.aks.mcrsvc.calculator;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j
 public class CalculatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        log.debug("CalculatorApplication started ");
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
 
 }
