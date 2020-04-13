b37b9eaab51754a958fb48cd897306dffa3eaedf
AakankshaKaskar <akankshakaskar3108@gmail.com>
2020-04-13 15:32:23 -0400

    Test cases added

---
+++ calculator/src/test/java/edu/aks/mcrsvc/calculator/svc/CalculatorSvcImplTest.java
@@ -0,0 +1,6 @@
+package edu.aks.mcrsvc.calculator.svc;
+
+
+
+public class CalculatorSvcImplTest {
+}
---
+++ calculator/src/test/java/edu/aks/mcrsvc/calculator/util/CommonUtilsTest.java
@@ -0,0 +1,32 @@
+package edu.aks.mcrsvc.calculator.util;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CommonUtilsTest {
+
+    CommonUtils commonUtils;
+    List<String> values;
+
+    @Before
+    public void before() {
+        commonUtils = new CommonUtils();
+        values = Arrays.asList("1", "2", "3", "4");
+    }
+
+    @Test
+    public void testConvert() {
+        List<Double> actualResult = commonUtils.convert(values);
+        //expected output calculation
+        List<Double> expectedResult = new ArrayList<>();
+        for (String s : values) {
+            expectedResult.add(Double.parseDouble(s));
+        }
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+}
